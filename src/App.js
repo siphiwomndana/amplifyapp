936a541af20b6828eb350775ca9e8b127a433964
Siphiwo Mndana <smndana@oldmutual.com>
2021-12-27 20:23:01 +0200

    Added second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!!
+          Testing this website for Oscar!!
         </p>
         <a
           className="App-link"
